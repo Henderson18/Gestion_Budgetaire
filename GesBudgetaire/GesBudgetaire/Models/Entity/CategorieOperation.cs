ac22d26b19c4a776ed0065f281cb1adfbe9816df
Henderson18 <hendersontmg18@gmail.com>
2022-02-20 15:34:45 +0100

    Entity framework Initialisation des entités et du DbContext pour le mapping Objet Relationnel

---
+++ GesBudgetaire/GesBudgetaire/Models/Entity/CategorieOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GesBudgetaire.Models
+{
+    public partial class CategorieOperation
+    {
+        public CategorieOperation()
+        {
+            Depense = new HashSet<Depense>();
+            Revenu = new HashSet<Revenu>();
+        }
+
+        public int Idcategorie { get; set; }
+        public string Libelle { get; set; }
+
+        public virtual ICollection<Depense> Depense { get; set; }
+        public virtual ICollection<Revenu> Revenu { get; set; }
+    }
+}
---
+++ GesBudgetaire/GesBudgetaire/Models/Entity/Depense.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GesBudgetaire.Models
+{
+    public partial class Depense
+    {
+        public int Iddepense { get; set; }
+        public int? Idutilisateur { get; set; }
+        public int? Idrevenu { get; set; }
+        public int? Idcategorie { get; set; }
+        public double? Montant { get; set; }
+        public DateTime? Date { get; set; }
+
+        public virtual CategorieOperation IdcategorieNavigation { get; set; }
+        public virtual Revenu IdrevenuNavigation { get; set; }
+        public virtual Utilisateur IdutilisateurNavigation { get; set; }
+    }
+}
